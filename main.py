f7012dfb234a5dec3af14e3283a64493949892e0
av215 <79746477+av215@users.noreply.github.com>
2022-05-28 21:18:45 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-# Check if two words are anagrams 
-# Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
+class Student:
+    name = ""
+    age = ""
+    track = ["full", "part", "contract"]
+    score = ""
 
-# Get string inputs
-word = input("Enter the first string : ")
-anagram = input("Enter the second string : ")
+    # [assignment] Skeleton class. Add your code here
+    def __init__():
+       # self.name = name
+        #self.age = age
+       # self.track = track
+       # self.score = score
+        #pass
 
-# convert both the strings into lowercase
-word = word.lower()
-anagram = anagram.lower()
+    def change_name(name):
+        name = name
+        return name
 
+    def change_age(age):
+        age = age
+        return age
 
-def find_anagram(word, anagram):
-    # [assignment] Add your code here
-    return sorted(word) == sorted(anagram)
-    #return True
-    
-if find_anagram(word, anagram):
-    print("Strings are anagram")
-else:
-    print("Strings are not anagram")
+    def add_track(track):
+        track = track.append(track)
+
+    def get_score(score):
+        return score
+
+Bob = Student("Bob", 26, "part", 20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
